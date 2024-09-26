3ea7f9110267a1d9226d9f03528e0348026ac354
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-09-26 13:01:55 +0000

    Get rid of the clunky page state detection.

--- transport/internet/browser_dialer/dialer.js
+++ transport/internet/browser_dialer/dialer.js
@@ -2,21 +2,4 @@
 
 "use strict";
 
-let main = async () => {
-	// Runs in the main scope after the page becomes interactive
-};
-
-switch (document.readyState) {
-	case "loading": {
-		document.addEventListener("readystatechange", (ev) => {
-			if (document.readyState == "interactive") {
-				main();
-			};
-		});
-		break;
-	};
-	case "interactive":
-	case "complete": {
-		main();
-	};
-};
+// All code below runs in the main scope after the page becomes interactive
